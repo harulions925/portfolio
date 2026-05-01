201d08577e41ac5c578a7c6aa1ff2a8673eb3efc
harulions925 <haru.lions925@gmail.com>
2026-05-01 23:07:37 +0900

    Add study log creation

--- app/controllers/study_logs_controller.rb
+++ app/controllers/study_logs_controller.rb
@@ -1,13 +1,29 @@
 class StudyLogsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @study_logs = StudyLog.all
   end
 
   def new
-    @user = User.new
+    @study_log = StudyLog.new
   end
 
   def create
+    @study_log = current_user.study_logs.new(study_log_params)
+
+    if @study_log.save
+      flash[:notice] = "新規投稿の登録完了しました"
+      redirect_to study_logs_path
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
+
+  private
   
+  def study_log_params
+    params
+      .require(:study_log)
+      .permit(:title, :content, :study_time)
+  end
 end
--- app/models/study_log.rb
+++ app/models/study_log.rb
@@ -1,2 +1,3 @@
 class StudyLog < ApplicationRecord
+    belongs_to :user
 end
