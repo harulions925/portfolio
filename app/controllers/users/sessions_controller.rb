5f120cd6200dac434498cd19ef1190a11dbf568b
harulions925 <haru.lions925@gmail.com>
2026-05-06 19:02:36 +0900

    Fix edit page title and guest login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
-
   def guest_sign_in
-    user = User.find_or_create_by(email: "guest@example.com") do |user|
+    user = User.find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.name = "ゲストユーザー" # ←ユーザー名を設定している場合は追加
+      user.name = "ゲストユーザー"
     end
-    sign_in user # ← Deviseのログインメソッド
+
+    sign_in user
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました"
   end
-
-  # ・・・ (Rails7へDviseを導入する際に追記した内容) ・・・
-
 end
